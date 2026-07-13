9a3b0959563c99821a50a01d6b0ae2c504132064
Lydia Bollig <raven.fayrin@gmail.com>
2026-07-13 14:27:00 -0400

    added middleware for metrics counting

--- main.go
+++ main.go
@@ -3,14 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+fileserverHits atomic.Int32
+}
+
+var cfg apiConfig
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
 	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
