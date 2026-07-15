575d27333bb036ce93b3a869f1716b024cd8a042
Lydia Bollig <raven.fayrin@gmail.com>
2026-07-15 13:44:41 -0400

    added database fully and offically

--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"Chirpy/internal/database"
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries 
 }
 
 func main() {
+	godotenv.Load()
 	const filepathRoot = "."
 	const port = "8080"
+	dbURL := os.Getenv("DB_URL")
+	data, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db: database.New(data),
 	}
 
 	mux := http.NewServeMux()
