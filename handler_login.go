9605abad9b56325ff238919f855abdda0e43a052
Lydia Bollig <raven.fayrin@gmail.com>
2026-07-20 13:43:13 -0400

    Added login functionality

---
+++ handler_login.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"Chirpy/internal/auth"
+	"encoding/json"
+	"net/http"
+)
+
+
+
+func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Password string `json:"password"`
+		Email string `json:"email"`
+	}
+	type response struct {
+		User
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	user, err := cfg.db.GetUserByEmail(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
+		return
+	}
+
+	match, err := auth.CheckPasswordHash(params.Password, user.HashedPassword)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
+		return
+	}
+	if match == false {
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
+		return
+	} else {
+		respondWithJSON(w, http.StatusOK, response{
+		User: User{
+			ID:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
+	})
+	}	
+}
