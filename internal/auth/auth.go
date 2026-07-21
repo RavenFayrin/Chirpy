bad18bce6b7401e6af68575becb5c1ce96a92c52
Lydia Bollig <raven.fayrin@gmail.com>
2026-07-21 12:43:18 -0400

    added JWT

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/google/uuid"
 )
 
 // HashPassword -
@@ -20,4 +25,45 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	mySigningKey := []byte(tokenSecret)
+
+	claims := &jwt.RegisteredClaims{
+		Issuer: "chirpy-access",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject: userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(mySigningKey)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	}, jwt.WithLeeway(5*time.Second))
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	claimUserId, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userId, err := uuid.Parse(claimUserId)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return userId, nil
 }
