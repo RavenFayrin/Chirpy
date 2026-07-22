9fe709b81d4c3745e3dceec810e3bdaff8a60116
RavenFayrin <raven.fayrin@gmail.com>
2026-07-22 16:47:54 -0400

    made to match lesson

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -60,20 +60,19 @@ func (q *Queries) GetUserByEmail(ctx context.Context, email string) (User, error
 }
 
 const updateUser = `-- name: UpdateUser :one
-UPDATE users SET hashed_password = $1,
-email = $2
-WHERE id = $3
+UPDATE users SET email = $2, hashed_password = $3, updated_at = NOW()
+WHERE id = $1
 RETURNING id, created_at, updated_at, email, hashed_password
 `
 
 type UpdateUserParams struct {
-	HashedPassword string
-	Email          string
 	ID             uuid.UUID
+	Email          string
+	HashedPassword string
 }
 
 func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error) {
-	row := q.db.QueryRowContext(ctx, updateUser, arg.HashedPassword, arg.Email, arg.ID)
+	row := q.db.QueryRowContext(ctx, updateUser, arg.ID, arg.Email, arg.HashedPassword)
 	var i User
 	err := row.Scan(
 		&i.ID,
